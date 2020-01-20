ebec17fe037651c3320e377a90b97690425074e9
Gyoma <art.golovin.v@gmail.com>
2020-01-20 20:00:36 +0700

    Update det.cpp

--- det.cpp
+++ det.cpp
@@ -5,7 +5,7 @@
 
 
 //with extra memory allocation (minors)
-double _det(double** matrix, const uint32_t size)
+double det_1(double** matrix, const uint32_t size)
 {
 
     if (size == 1)
@@ -41,13 +41,13 @@ double _det(double** matrix, const uint32_t size)
 
     double result = 0.0;
     for (uint32_t i = 0; i < size; ++i)
-        result += ((i + 1) & 1 ? 1 : -1) * matrix[0][i] * _det(minor(0, i).get(), size - 1);
+        result += ((i + 1) & 1 ? 1 : -1) * matrix[0][i] * det_1(minor(0, i).get(), size - 1);
 
     return result;
 }
 
 //without extra memory allocation
-double det(double** matrix, const uint32_t size)
+double det_2(double** matrix, const uint32_t size)
 {
     if (size == 1)
         return matrix[0][0];
@@ -77,9 +77,83 @@ double det(double** matrix, const uint32_t size)
             }
 
         return result;
-    }
+        }
     };
 
     std::unique_ptr<bool> check_column_array(new bool[size]());
     return determinant(matrix, size, 0, 0, size - 1, check_column_array.get());
+}
+
+//without extra memory allocation & non-recursive function
+//the fastest one
+double det_3(double** matrix, const uint32_t size)
+{
+    auto nextReshuffle = [&size](int32_t* arr) -> bool
+    {
+
+        int32_t i = size - 2;
+        uint32_t j = 0;
+
+        while (i > -1 && arr[i] > arr[i + 1])
+            i--;
+
+        if (i > -1)
+        {
+
+            j = i + 1;
+
+            while (j < size && arr[j + 1] > arr[i])
+                j++;
+
+            //swap(arr[i], arr[j]);
+            arr[i] ^= arr[j] ^= arr[i] ^= arr[j];
+
+            for (j = i + 1; j <= (size + i - 1) / 2; j++)
+            {
+                //swap(arr[j], arr[dimension - j + i]);
+                arr[j] ^= arr[size - j + i] ^= arr[j] ^= arr[size - j + i];
+            }
+
+            return true;
+        }
+
+        return false;
+    };
+
+
+    double determinant = 0.0;
+
+    //the array of indexes for inversions counting
+    int32_t* indexes = new int32_t[size]();
+
+    for (size_t i = 0; i < size; i++)
+        indexes[i] = i;
+
+    do
+    {
+        //number of inversions
+        uint32_t inversions = 0;
+
+        double term = 1.0;
+
+        //inversions counting
+        for (size_t j = 0; j < size - 1; j++)
+            for (size_t k = j + 1; k < size; k++)
+                if (indexes[j] > indexes[k])
+                    inversions++;
+
+        for (size_t j = 0; j < size; j++)
+            term *= matrix[indexes[j]][j];
+
+        //odd part in the determinant sum must be multiplied by -1
+        if (inversions & 1)
+            term *= -1;
+
+        determinant += term;
+
+    } while (nextReshuffle(indexes));
+
+    delete[] indexes;
+
+    return determinant;
 }
